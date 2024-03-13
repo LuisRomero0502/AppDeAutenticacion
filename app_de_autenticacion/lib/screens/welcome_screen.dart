aedb95bc2d0e920ff6b04664cf963580bf41b505
LuisRomero05 <lromeroi@unah.hn>
2024-03-13 16:34:30 -0600

    Pantalla de Inicio
    Creacion de los colores de la app, creacion de la pantalla de inicio, ubicacion de botones(FALTAN POR PROGRAMAR, DAN ERROR)

---
+++ app_de_autenticacion/lib/screens/welcome_screen.dart
@@ -0,0 +1,74 @@
+import 'package:app_de_autenticacion/theme/theme.dart';
+import 'package:app_de_autenticacion/widgets/custom_scaffold.dart';
+import 'package:app_de_autenticacion/widgets/welcome_button.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(
+              flex: 8,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 0,
+                  horizontal: 40.0,
+                ),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                            text: '¡Bienvenido de nuevo!\n',
+                            style: TextStyle(
+                              fontSize: 45.0,
+                              fontWeight: FontWeight.w600,
+                            )),
+                        TextSpan(
+                            text:
+                                '\nIngrese datos personales a su cuenta de empleado',
+                            style: TextStyle(
+                              fontSize: 20,
+                              // height: 0,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+          Flexible(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Iniciar sesión',
+                      //Sign in, accion
+                      color: Colors.transparent,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Inscribirse',
+                      //Sign up, accion
+                      color: Colors.white,
+                      textColor: lightColorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
